fd0ae29c1a8e80d0551afcae1524da9bbdb100f1
Wh0Kn0w5 <Wh0r34llyc4r35@Gmail.com>
2023-04-03 21:19:40 -0500

    Finalizing push for OV into CV framework.

--- Modules/Projects/Core/Scene_ForestAdept_Custom.lua
+++ Modules/Projects/Core/Scene_ForestAdept_Custom.lua
@@ -5,5 +5,5 @@ local function IsChipVoreFatal()
 end
 
 function ChipCVFatalCheck()
-    return Storage.GetNumber ("WRC_CHIP_CVRELEASE") < GetTimeHourTotal()+4
+    return Storage.GetNumber ("WRC_CHIP_CVRELEASE") < (GetTimeHourTotal()+4)
 end
